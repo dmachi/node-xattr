34100dc1155effb0ffe3a83bec1745c11bfe671f
Dustin Machi <dmachi@dojotoolkit.org>
2012-11-15 17:59:32 -0500

    updates to async that don't work

--- src/node_xattr.h
+++ src/node_xattr.h
@@ -1,13 +1,14 @@
 #include <node.h>
 #include <uv.h>
 #include <sys/time.h>
+#include <string.h>
 #include <node_version.h>
 
 struct AsyncBaton {
     uv_work_t request;
     v8::Persistent<v8::Function> callback;
     int error;
-
+    
     v8::String filename;
     v8::String attribute;
     v8::String value;
